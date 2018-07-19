1f126bdb923edbaffb9bb04bdc30dfd1da128571
buer2202 <buer2202@163.com>
2018-07-19 15:17:01 +0800

    更新资产异常文件

--- src/Exceptions/AssetException.php
+++ src/Exceptions/AssetException.php
@@ -6,10 +6,5 @@ use Exception;
 
 class AssetException extends Exception
 {
-    public function errorMessage()
-    {
-        $errorMsg = '异常文件: ' . $this->getFile() . ' 行: ' . $this->getLine() . ' 信息: ' . $this->getMessage();
-
-        return $this->getMessage();
-    }
+    //
 }
